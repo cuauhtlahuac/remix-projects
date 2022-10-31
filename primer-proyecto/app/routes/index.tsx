f9279eb11740128bcf88b5673299765c7c3dae7c
Cuauhtlahuac macbookpro <cuitlagua@gmail.com>
2022-10-30 23:24:44 -0600

    heroes exercise done

--- primer-proyecto/app/routes/index.tsx
+++ primer-proyecto/app/routes/index.tsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react";
+
 export default function Index() {
+  useEffect(() => {
+    window.location.href = `${window.location.href}landing-page`
+  }, [])
+
+
   return (
-    <div>hola</div>
+    <div>HEROES PAGE</div>
   );
 }
---
+++ primer-proyecto/app/routes/landing-page.tsx
@@ -0,0 +1,54 @@
+import { useLoaderData } from '@remix-run/react';
+import styles from '~/styles/app.css';
+
+export const links = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ];
+};
+
+export const meta = async () => {
+    return {
+        title: 'Landing page',
+        description: 'This is a landing page',
+    };
+};
+
+export const action = async ({ request, params }) => {
+    return null;
+};
+
+export const loader = async ({ request, params }) => {
+    const characters = await (await fetch("https://rickandmortyapi.com/api/character")).json();
+    return characters;
+};
+
+export default function LandingPage() {
+    const { results } = useLoaderData();
+    const [{
+        name,
+        species,
+        gender,
+        origin,
+        location,
+        image
+    }] = results
+    return (
+        <section className='hero text'>
+            <div className="text">
+                <h1>Soy {name}</h1>
+                <p>{origin?.name}</p>
+                <p>{location?.name}</p>
+                <p>{species}</p>
+                <p>{gender}</p>
+            </div>
+            <figure className='illustration'>
+                <img src={image} />
+            </figure>
+            <button className='cta'>{`VAMOS A ${origin?.name}!`}</button>
+        </section >
+    );
+}
