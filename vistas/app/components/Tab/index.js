9cb9cd12d07ec7933fc54d7c132bb3a38f62f9ef
Cuauhtlahuac macbookpro <cuitlagua@gmail.com>
2022-11-14 19:09:42 -0600

    advaces third exercise

--- vistas/app/components/Tab/index.js
+++ vistas/app/components/Tab/index.js
@@ -1,36 +1,69 @@
-import React, { useContext, useState } from 'react';
-export const ThemeContext = React.createContext([]);
+import React, { useContext, useRef, useState } from 'react';
+export const ThemeContext = React.createContext();
+
+const hoverStyles = {
+    container: {
+        background: '#0002',
+        borderInline: 'white 1px solid ',
+    }
+}
 
 const Tab = ({ children, index, onPress }) => {
-    const data = useContext(ThemeContext);
+    const { data } = useContext(ThemeContext);
     const [overStyles, setOverStyles] = useState({})
+    const [active, setActive] = useState(false)
+    const divRef = useRef(index);
+
+    const handleOnPress = () => {
+        const currentActive = divRef.current.id == index;
+        setActive(currentActive)
+        onPress(data?.[index])
+        if (currentActive) {
+            setOverStyles({ hoverStyles })
+        } else {
+            setOverStyles({})
+            setActive(false);
+        }
+    }
+
+    const handleMouseLeave = () => {
+        if (!active) {
+            setOverStyles({});
+            setActive(false);
+        }
+    }
     return (
         <div
+            id={index}
+            ref={divRef}
             style={{
                 display: 'flex',
                 flex: 1,
                 justifyContent: 'center',
                 cursor: 'pointer',
-                ...overStyles
+                borderInline: '#002244 1px solid ',
+                borderRadius: 10,
+                ...overStyles.container,
             }}
-            onMouseOver={() => setOverStyles({ background: '#0002' })}
-            onMouseLeave={() => setOverStyles({})}
-            onClick={() => onPress(data[index])}
+            onMouseOver={() => setOverStyles({ ...overStyles, ...hoverStyles })}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleOnPress}
         >
-            <h3 style={{ color: 'blue' }}>{children}</h3>
+            <h3 style={{ color: 'white' }}>{children}</h3>
         </div>
     )
 }
 
 const Group = ({ children, data }) => {
     return (
-        <ThemeContext.Provider value={data}>
+        <ThemeContext.Provider value={{ data }}>
             <div style={{
-                border: '1px solid red',
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'space-between',
                 width: '100%',
+                background: 'linear-gradient(70deg, #007FFF, #0066b2);',
+                padding: 20,
             }}>
                 {children}
             </div>
@@ -41,16 +74,18 @@ const Group = ({ children, data }) => {
 const List = ({ children }) => (
     <div style={{
         display: 'flex',
-        width: '100%',
+        flex: 1,
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
+        background: '#034694',
+        borderRadius: 10,
     }}>
         {children}
     </div>)
 
 const Panels = ({ children }) => (
-    <div style={{ background: 'red' }}>
+    <div style={{ background: 'white', borderRadius: 10, padding: 10, marginTop: 10 }}>
         {children}
     </div>)
 
@@ -65,7 +100,7 @@ const Panel = ({ children }) => {
     )
 }
 
-export default Tab
+export default Tab;
 
 Tab.Group = Group;
 Tab.List = List;
--- vistas/app/routes/index.tsx
+++ vistas/app/routes/index.tsx
@@ -1,7 +1,8 @@
 import { LoaderFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { useContext, useState } from 'react';
-import { Tab, ThemeContext } from '~/components'
+import { useState } from 'react';
+
+import { Tab } from '~/components'
 import { createTabs } from '~/mocks'
 
 export const loader: LoaderFunction = () => {
@@ -11,7 +12,6 @@ export const loader: LoaderFunction = () => {
 export default function Index() {
   const data = useLoaderData();
   const [tabData, setTabData] = useState(data[0])
-  console.log({ tabData })
   const handleOnSelected = (selected: any) => {
     setTabData(selected)
   }
@@ -23,15 +23,22 @@ export default function Index() {
             {
               header: string,
             }, index: number) => (
-            <Tab index={index} key={header} onPress={handleOnSelected}>{header}</Tab>
+            <Tab
+              index={index}
+              key={header}
+              onPress={handleOnSelected}
+            >
+              {header}
+            </Tab>
           ))
           }
         </Tab.List>
         <Tab.Panels>
-          {tabData.body.map(({ title, detail }) => (<Tab.Panel>
-            <h3>{title}</h3>
-            <p>{detail}</p>
-          </Tab.Panel>))}
+          {tabData.body.map(({ title, detail }: { [key: string]: string }) => (
+            <Tab.Panel>
+              <h3>{title}</h3>
+              <p>{detail}</p>
+            </Tab.Panel>))}
         </Tab.Panels>
       </Tab.Group>
     </div>
