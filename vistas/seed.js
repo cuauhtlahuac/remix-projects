7944ca2da101cf1e8fb22390c107767367413f8a
Urbano Cerón <japerez@upexternos.com.mx>
2022-11-27 21:00:10 -0600

    fix: update seed * and Product Schema

--- vistas/seed.js
+++ vistas/seed.js
@@ -1,30 +1,40 @@
-/* import { faker } from '@faker-js/faker'
-import { PrismaClient } from '@prisma/client' */
-
 const { faker } = require('@faker-js/faker');
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
+const numberOfItems = 500;
 
 const getProduct = () => {
     const name = faker.commerce.productName();
+    const slug = name + faker.phone.imei();
     return {
-        slug: faker.helpers.slugify(name),
+        slug,
         name,
         body: faker.commerce.productDescription(),
         images: [
             faker.image.imageUrl(),
-            faker.image.imageUrl(),
-            faker.image.imageUrl(),
+            faker.image.abstract(),
+            faker.image.food(),
         ],
         color: faker.color.human(),
+        price: faker.commerce.price(1, 1000),
+        stars: Math.floor(Math.random() * (5 - 1) + 1),
     }
 }
 
-const saveInDB = async () => await prisma.product.create({ data: getProduct() });
+const seed = () => {
+    return [...Array(numberOfItems).keys()].map(getProduct);
+}
+const seeded = seed();
 
-const seed = async () => {
-    await Promise.all([...Array(10).keys()].map(saveInDB));
+const saveInDB = async () => await prisma.product.createMany({ data: seeded });
+
+const asyncHandler = async () => {
+    try {
+        await saveInDB();
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-seed();
+asyncHandler().then(() => console.log("DONE!")).catch((error) => console.log("ERROR", error));
